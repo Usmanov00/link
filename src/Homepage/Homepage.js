d0fcd3c9788522b971fca191e91b2f7d37d43e9f
Usmanov <roma.maksato@gmail.com>
2022-10-13 23:45:20 +0600

    change background

--- src/Homepage/Homepage.js
+++ src/Homepage/Homepage.js
@@ -8,7 +8,6 @@ const Homepage = () => {
         <div className="links">
           <ul>
             <li><a href="https://wa.me/+996507070609" className="link">Сайт</a></li>
-            <br/><br/>
             <li><a href="https://2gis.kg/bishkek/search/fen/firm/70000001024590847?m=74.575869%2C42.871204%2F19.97"
                    className="link">Молодая гвардия (Адресс на карте)
               <span><br/>Главный фелиал</span>
@@ -20,24 +19,25 @@ const Homepage = () => {
               </a>
               <p/>
             </li>
-            <br/>
-            <li>
-              {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-              <a href="#" className="link"> LinkedIn</a>
+            <li className="itemsCenter itemsInBox">
+              <img src={whatsapp} alt="" width={30}/>
+              <a href="https://wa.me/+996706609030" className="link"> Молодая Гвардия
+              </a>
+              <p/>
             </li>
-            <li>
-              {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-              <a href="#" className="link"> Codepen</a>
+            <li className="itemsCenter itemsInBox">
+              <img src={whatsapp} alt="" width={30}/>
+              <a href="https://wa.me/+996706609030" className="link"> Молодая Гвардия
+              </a>
+              <p/>
             </li>
-            <li>
-              {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-              <a href="#" className="link"> My website</a>
+            <li className="itemsCenter itemsInBox">
+              <img src={whatsapp} alt="" width={30}/>
+              <a href="https://wa.me/+996706609030" className="link"> Молодая Гвардия
+              </a>
+              <p/>
             </li>
           </ul>
-          <div className="card">
-            <h3>Card</h3>
-            <p>Just a card to display some information ...</p>
-          </div>
         </div>
       </div>
     </div>
